0e5f997b9490e07a69353dab55ba7fc11d9bd216
Azad Uddin <azad@antlerinteractive.com>
2024-08-07 15:56:31 +0800

    final contract with comments

--- GenesisNFT/contracts/GenesisNFT.sol
+++ GenesisNFT/contracts/GenesisNFT.sol
@@ -14,57 +14,64 @@ contract GenesisNFT is
     ERC1155Burnable,
     ERC1155Supply
 {
-    // Counter for the next token ID
-    uint256 public nextTokenId = 0;
-
-    // Constructor that sets the initial owner and initial base URI
+    // Array to store all minted token IDs
+    uint256[] private _allTokenIds;
+    
+    /**
+     * @dev Constructor that sets the initial owner and initial base URI.
+     * @param initialOwner Address of the initial owner.
+     * @param initialBaseURI Base URI for the token metadata.
+     */
     constructor(address initialOwner, string memory initialBaseURI) ERC1155(initialBaseURI) Ownable(initialOwner) {
        
     }
 
-    // Function to update the base URI
+    /**
+     * @dev Function to update the base URI.
+     * @param newuri New URI to set.
+     */
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
-    // Function to pause all token transfers
+    /**
+     * @dev Function to pause all token transfers.
+     */
     function pause() public onlyOwner {
         _pause();
     }
 
-    // Function to unpause all token transfers
+    /**
+     * @dev Function to unpause all token transfers.
+     */
     function unpause() public onlyOwner {
         _unpause();
     }
 
-    // Mint function
+    /**
+     * @dev Mint function to create new tokens.
+     * @param account Address of the recipient.
+     * @param id Token ID to mint.
+     * @param amount Amount of tokens to mint.
+     * @param data Additional data with no specified format.
+     */
     function mint(
         address account,
+        uint256 id,
         uint256 amount,
         bytes memory data
     ) public onlyOwner {
-        uint256 tokenId = nextTokenId;
-        nextTokenId++;
-        _mint(account, tokenId, amount, data);
+        _mint(account, id, amount, data);
+        _addTokenId(id);
     }
 
-    // Batch mint function to mint multiple tokens at once
-    // function mintBatch(
-    //     address to,
-    //     uint256[] memory amounts,
-    //     bytes memory data
-    // ) public onlyOwner {
-    //     uint256[] memory tokenIds = new uint256[](amounts.length);
-
-    //     for (uint256 i = 0; i < amounts.length; i++) {
-    //         tokenIds[i] = nextTokenId;
-    //         nextTokenId++;
-    //     }
-
-    //     _mintBatch(to, tokenIds, amounts, data);
-    // }
-
-    // Batch mint function to mint multiple tokens at once
+    /**
+     * @dev Batch mint function to mint multiple tokens at once.
+     * @param to Address of the recipient.
+     * @param ids Array of token IDs to mint.
+     * @param amounts Array of amounts of tokens to mint for each ID.
+     * @param data Additional data with no specified format.
+     */
     function mintBatch(
         address to,
         uint256[] memory ids,
@@ -72,9 +79,37 @@ contract GenesisNFT is
         bytes memory data
     ) public onlyOwner {
         _mintBatch(to, ids, amounts, data);
+        _addTokenIds(ids);
     }
 
-    // Efficient batch transfer function
+    /**
+     * @dev Function to add a token ID to the list of all tokens.
+     * @param id Token ID to add.
+     */
+    function _addTokenId(uint256 id) internal {
+        if (!exists(id)) {
+            _allTokenIds.push(id);
+        }
+    }
+
+    /**
+     * @dev Function to add multiple token IDs to the list of all tokens.
+     * @param ids Array of token IDs to add.
+     */
+    function _addTokenIds(uint256[] memory ids) internal {
+        for (uint256 i = 0; i < ids.length; i++) {
+            _addTokenId(ids[i]);
+        }
+    }
+
+    /**
+     * @dev Efficient batch transfer function to transfer tokens to multiple addresses.
+     * @param from Address of the sender.
+     * @param to Array of recipient addresses.
+     * @param ids Array of token ID arrays to transfer.
+     * @param amounts Array of token amount arrays to transfer.
+     * @param data Additional data with no specified format.
+     */
     function safeBatchTransferFromBatch(
         address from,
         address[] calldata to,
@@ -89,15 +124,20 @@ contract GenesisNFT is
         }
     }
 
-    // Function to get all token IDs owned by an address
+    /**
+     * @dev Function to get all token IDs owned by an address.
+     * @param owner Address of the token owner.
+     * @return Array of token IDs owned by the address.
+     */
     function getOwnedTokens(address owner) public view returns (uint256[] memory) {
-        uint256 totalSupply = nextTokenId - 1;
-        uint256[] memory result = new uint256[](totalSupply);
+        uint256 totalTokenCount = _allTokenIds.length;
+        uint256[] memory result = new uint256[](totalTokenCount);
         uint256 counter = 0;
 
-        for (uint256 i = 1; i <= totalSupply; i++) {
-            if (balanceOf(owner, i) > 0) {
-                result[counter] = i;
+        for (uint256 i = 0; i < totalTokenCount; i++) {
+            uint256 id = _allTokenIds[i];
+            if (balanceOf(owner, id) > 0) {
+                result[counter] = id;
                 counter++;
             }
         }
@@ -111,9 +151,13 @@ contract GenesisNFT is
         return trimmedResult;
     }
 
-    // Override functions required by Solidity
-
-
+    /**
+     * @dev Override function required by Solidity to update token transfers.
+     * @param from Address of the sender.
+     * @param to Address of the recipient.
+     * @param ids Array of token IDs.
+     * @param amounts Array of token amounts.
+     */
     function _update(
         address from,
         address to,
